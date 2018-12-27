8e05ec7259fdfe9bfac25def8c4c1c9ebfa36b13
Hrishikesh Basak <insind5006@INSZOOMDEV.COM>
2018-12-27 11:32:13 +0530

    modifications1 in window

--- Regration_POM_PF/src/main/java/com/inszoom/regration/helper/window/Window.java
+++ Regration_POM_PF/src/main/java/com/inszoom/regration/helper/window/Window.java
@@ -1,7 +1,26 @@
 package com.inszoom.regration.helper.window;
 
-public class Window {
-	
-	
+import org.openqa.selenium.WebDriver;
 
+public class Window {
+
+	private WebDriver driver;
+
+	public Window(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void goBack() {
+		driver.navigate().back();
+	}
+
+	public void forward() {
+		driver.navigate().forward();
+	}
+
+	public void refresh() {
+		driver.navigate().refresh();
+	}
+	
+	
 }
