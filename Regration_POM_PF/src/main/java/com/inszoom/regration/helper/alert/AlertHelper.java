05f624cf61a55dcf321d6b799286dced1ea68232
insind5006 <insind5006@fqk7.INSZOOMDEV.COM>
2019-01-07 10:54:18 +0530

    modifications in wait and alert

--- Regration_POM_PF/src/main/java/com/inszoom/regration/helper/alert/AlertHelper.java
+++ Regration_POM_PF/src/main/java/com/inszoom/regration/helper/alert/AlertHelper.java
@@ -1,8 +1,11 @@
 package com.inszoom.regration.helper.alert;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
+
+import com.inszoom.regration.helper.logger.LoggerHelper;
 
 
 /**
@@ -11,42 +14,42 @@ import org.openqa.selenium.WebDriver;
 public class AlertHelper{
 	
 	private WebDriver driver;
-	//private Logger oLog = LoggerHelper.getLogger(AlertHelper.class);
+	private Logger oLog = LoggerHelper.getLogger(AlertHelper.class);
 	
 	public AlertHelper(WebDriver driver) {
 		this.driver = driver;
-		//oLog.debug("AlertHelper : " + this.driver.hashCode());
+		oLog.debug("AlertHelper : " + this.driver.hashCode());
 	}
 	
 	public Alert getAlert() {
-		//oLog.debug("");
+		oLog.debug("");
 		return driver.switchTo().alert();
 	}
 	
 	public void AcceptAlert() {
-		//oLog.info("");
+		oLog.info("");
 		getAlert().accept();
 	}
 	
 	public void DismissAlert() {
-		//oLog.info("");
+		oLog.info("");
 		getAlert().dismiss();
 	}
 
 	public String getAlertText() {
 		String text = getAlert().getText();
-		//oLog.info(text);
+		oLog.info(text);
 		return text;
 	}
 
 	public boolean isAlertPresent() {
 		try {
 			driver.switchTo().alert();
-			//oLog.info("true");
+			oLog.info("true");
 			return true;
 		} catch (NoAlertPresentException e) {
 			// Ignore
-			//oLog.info("false");
+			oLog.info("false");
 			return false;
 		}
 	}
--- Regration_POM_PF/src/main/java/com/inszoom/regration/helper/wait/WaitHelper.java
+++ Regration_POM_PF/src/main/java/com/inszoom/regration/helper/wait/WaitHelper.java
@@ -2,11 +2,20 @@ package com.inszoom.regration.helper.wait;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+
+import com.inszoom.regration.helper.logger.LoggerHelper;
 
 public class WaitHelper {
 
 	private WebDriver driver;
+	private Logger oLog = LoggerHelper.getLogger(WaitHelper.class);
+
+	public WaitHelper(WebDriver driver) {
+		this.driver = driver;
+		oLog.debug("AlertHelper : " + this.driver.hashCode());
+	}
 
 	public void setImplicitWait(long timeout, TimeUnit unit) {
 
