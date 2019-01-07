dc4e2815ce5c97cbe6332508a42bdb869943cfdc
insind5006 <insind5006@fqk7.INSZOOMDEV.COM>
2019-01-07 19:07:07 +0530

    WaitHelper modifications

--- Regration_POM_PF/src/main/java/com/inszoom/regration/helper/wait/WaitHelper.java
+++ Regration_POM_PF/src/main/java/com/inszoom/regration/helper/wait/WaitHelper.java
@@ -3,7 +3,14 @@ package com.inszoom.regration.helper.wait;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.ElementNotVisibleException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.NoSuchFrameException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.inszoom.regration.helper.logger.LoggerHelper;
 
@@ -20,6 +27,39 @@ public class WaitHelper {
 	public void setImplicitWait(long timeout, TimeUnit unit) {
 
 		driver.manage().timeouts().implicitlyWait(timeout, unit == null ? TimeUnit.SECONDS : unit);
+	}
+
+	public void setPageLoadTimeout(long timeout, TimeUnit unit) {
+		oLog.info(timeout);
+		driver.manage().timeouts().pageLoadTimeout(timeout, unit == null ? TimeUnit.SECONDS : unit);
+	}
+
+	private WebDriverWait getWait(int timeOutInSeconds, int pollingEveryInMiliSec) {
+		oLog.debug("WebDriverWait");
+		WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+		wait.pollingEvery(pollingEveryInMiliSec, TimeUnit.MILLISECONDS);
+		wait.ignoring(NoSuchElementException.class);
+		wait.ignoring(ElementNotVisibleException.class);
+		wait.ignoring(StaleElementReferenceException.class);
+		wait.ignoring(NoSuchFrameException.class);
+		return wait;
+	}
+
+	public void waitForElementVisible(WebElement locator, int timeOutInSeconds, int pollingEveryInMiliSec) {
+		oLog.info(locator);
+		WebDriverWait wait = getWait(timeOutInSeconds, pollingEveryInMiliSec);
+		wait.until(ExpectedConditions.visibilityOf(locator));
+	}
+	
+	public void waitForElement(WebDriver driver, WebElement element, long timeout) {
+		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		wait.until(ExpectedConditions.visibilityOf(element));
+		oLog.info("element found..."+ element.getText());
+	}
+	
+	public WebElement waitForElement(WebDriver driver,long time,WebElement element){
+		WebDriverWait wait = new WebDriverWait(driver, time);
+		return wait.until(ExpectedConditions.elementToBeClickable(element));
 	}
 
 }
