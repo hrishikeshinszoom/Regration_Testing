7e7ce644e52b08110274e13db2f79f2148d4c645
insind5006 <insind5006@fqk7.INSZOOMDEV.COM>
2019-01-15 15:54:06 +0530

    JavaScriptHelper

--- Regration_POM_PF/src/main/java/com/inszoom/regration/helper/javaScript/JavaScriptHelper.java
+++ Regration_POM_PF/src/main/java/com/inszoom/regration/helper/javaScript/JavaScriptHelper.java
@@ -1,7 +1,9 @@
 package com.inszoom.regration.helper.javaScript;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.inszoom.regration.helper.logger.LoggerHelper;
 
@@ -11,7 +13,7 @@ import com.inszoom.regration.helper.logger.LoggerHelper;
  *
  */
 public class JavaScriptHelper {
-	
+
 	private WebDriver driver;
 	private Logger oLog = LoggerHelper.getLogger(JavaScriptHelper.class);
 
@@ -19,6 +21,62 @@ public class JavaScriptHelper {
 		this.driver = driver;
 		oLog.debug("JavaScriptHelper : " + this.driver.hashCode());
 	}
-	
-	
+
+	public Object executeScript(String script) {
+		JavascriptExecutor exe = (JavascriptExecutor) driver;
+		oLog.info(script);
+		return exe.executeScript(script);
+	}
+
+	public Object executeScript(String script, Object... args) {
+		JavascriptExecutor exe = (JavascriptExecutor) driver;
+		oLog.info(script);
+		return exe.executeScript(script, args);
+	}
+
+	public void scrollToElemet(WebElement element) {
+		executeScript("window.scrollTo(arguments[0],arguments[1])", element.getLocation().x, element.getLocation().y);
+		oLog.info(element);
+	}
+
+	public void scrollToElemetAndClick(WebElement element) {
+		scrollToElemet(element);
+		element.click();
+		oLog.info(element);
+	}
+
+	public void scrollIntoView(WebElement element) {
+		executeScript("arguments[0].scrollIntoView()", element);
+		oLog.info(element);
+	}
+
+	public void scrollIntoViewAndClick(WebElement element) {
+		scrollIntoView(element);
+		element.click();
+		oLog.info(element);
+	}
+
+	public void scrollDownVertically() {
+		executeScript("window.scrollTo(0, document.body.scrollHeight)");
+	}
+
+	public void scrollUpVertically() {
+		executeScript("window.scrollTo(0, -document.body.scrollHeight)");
+	}
+
+	public void scrollDownByPixel() {
+		executeScript("window.scrollBy(0,1500)");
+	}
+
+	public void scrollUpByPixel() {
+		executeScript("window.scrollBy(0,-1500)");
+	}
+
+	public void ZoomInBypercentage() {
+		executeScript("document.body.style.zoom='40%'");
+	}
+
+	public void ZoomBy100percentage() {
+		executeScript("document.body.style.zoom='100%'");
+	}
 }
