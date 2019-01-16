4faf77266013bad042d4ee85481a82834adc6c1e
insind5006 <insind5006@fqk7.INSZOOMDEV.COM>
2019-01-16 11:11:49 +0530

    drop-down modifications

--- Regration_POM_PF/src/main/java/com/inszoom/regration/helper/dropdown/DropDownHelper.java
+++ Regration_POM_PF/src/main/java/com/inszoom/regration/helper/dropdown/DropDownHelper.java
@@ -1,5 +1,8 @@
 package com.inszoom.regration.helper.dropdown;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -26,11 +29,34 @@ public class DropDownHelper {
 		select.selectByVisibleText(visibleValue);
 		oLog.info("Locator : " + element + " Value : " + visibleValue);
 	}
-	
-//	public List<WebElement> getDropdownValues(String element){
-//		List<WebElement> list = driver.findElements(By.xpath(element));
-//		return list;
-//	}
-	
-	
+
+	public String getSelectedValue(WebElement element) {
+		String value = new Select(element).getFirstSelectedOption().getText();
+		oLog.info("WebElment :" + element + " value " + value);
+		return value;
+	}
+
+	public void selectUsingIndex(WebElement element, int index) {
+		Select select = new Select(element);
+		select.selectByIndex(index);
+		oLog.info("locator: " + element + "value:" + index);
+	}
+
+	public void selectUsingVisibleText(WebElement element, String text) {
+		Select select = new Select(element);
+		select.selectByVisibleText(text);
+		oLog.info("locator: " + element + "value:" + text);
+	}
+
+	public LinkedList<String> getAllDropDownList(WebElement locator) {
+		Select select = new Select(locator);
+		List<WebElement> elementList = select.getOptions();
+		LinkedList<String> valueList = new LinkedList<String>();
+
+		for (WebElement element : elementList) {
+			oLog.info(element.getText());
+			valueList.add(element.getText());
+		}
+		return valueList;
+	}
 }
