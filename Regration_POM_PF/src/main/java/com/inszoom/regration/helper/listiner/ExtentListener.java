ed5465ab6671a19c5497b000f383732bd9b5bb3e
insind5006 <insind5006@fqk7.INSZOOMDEV.COM>
2019-01-21 11:42:08 +0530

    21st jan 1st commit

---
+++ Regration_POM_PF/src/main/java/com/inszoom/regration/helper/listiner/ExtentListener.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.inszoom.regration.helper.listiner;
+
+import org.apache.log4j.Logger;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+import com.inszoom.regration.helper.logger.LoggerHelper;
+
+/**
+ * @author insind5006
+ *
+ */
+public class ExtentListener implements ITestListener {
+
+	private Logger olog = LoggerHelper.getLogger(ExtentListener.class);
+
+	public void onTestStart(ITestResult result) {
+		
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		
+	}
+
+	public void onTestFailure(ITestResult result) {
+		Reporter.log(result.getMethod().getMethodName() + "Test failed..." + result.getThrowable());
+		olog.info(result.getMethod().getMethodName() + "Test failed..." + result.getThrowable());
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		Reporter.log(result.getMethod().getMethodName() + "Test skipped..." + result.getThrowable());
+		olog.info(result.getMethod().getMethodName() + "Test skipped..." + result.getThrowable());
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+
+	}
+
+	public void onStart(ITestContext context) {
+		Reporter.log(context.getCurrentXmlTest().getName() + "Class Started...");
+		olog.info(context.getCurrentXmlTest().getName() + "Class Started....");
+	}
+
+	public void onFinish(ITestContext context) {
+		Reporter.log(context.getName() + "Test finished...");
+		olog.info(context.getName() + "Test finished....");
+
+	}
+
+}
---
+++ Regration_POM_PF/src/main/java/com/inszoom/regration/helper/listiner/Retry.java
@@ -0,0 +1,41 @@
+package com.inszoom.regration.helper.listiner;
+
+import org.apache.log4j.Logger;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+import com.inszoom.regration.helper.logger.LoggerHelper;
+
+public class Retry implements IRetryAnalyzer {
+	public int retryCount = 0;
+	public int maxRetryCount = 3;
+
+	private Logger olog = LoggerHelper.getLogger(Retry.class);
+
+	public boolean retry(ITestResult result) {
+		if (retryCount < maxRetryCount) {
+			olog.info("retrying test: " + result.getName() + "with status " + getResultStatusName(result.getStatus())
+					+ "for the " + (retryCount + 1) + "times. ");
+			retryCount++;
+			return true;
+		}
+		return false;
+	}
+
+	public String getResultStatusName(int Status) {
+		String resultName = null;
+		if (Status == 1) {
+			resultName = "SUCCESS";
+		}
+
+		if (Status == 2) {
+			resultName = "FALIURE";
+		}
+
+		if (Status == 3) {
+			resultName = "SKIP";
+		}
+
+		return resultName;
+	}
+}
